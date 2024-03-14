9bac729362f55556327e85be996aeda12b7da0ea
Chandadharani <163383244+Chandadharani@users.noreply.github.com>
2024-03-14 13:18:25 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,47 @@
+'''
+a=int(input("enter a value:"))
+b=int(input("enter a value:"))
+addition=a+b
+subtraction=a-b
+multiplication=a*b
+division=a/b
+floordivision=a//b
+modulation=a%b
+print("the addition of two values are:",addition)
+print("the subtraction of two values are:",subtraction)
+print("the multiplication of two values are:",multiplication)
+print("the division of two values are:",division)
+print("the floor division of two values are:",floordivision)
+print("the modulation of two values are:",modulation)
+'''
+
+
+
+a=bool(input("enter avalue:"))
+b=bool(input("enter a value:"))
+And=a and b
+Or=a or b
+Not=not a
+print("the result is:",And)
+print("the result is:",Or)
+print("thr result is:",Not)
+
+a=int(input("enter a value:"))
+b=int(input("enter a value:"))
+c=int(input("enter a value:"))
+d=int(input("enter a value:"))
+a+=b
+b-=c
+c*=a
+d**=c
+print("the value is :",a)
+print("the value is :",b)
+print("the value is :",c)
+print("the value is :",d)
+
+
+
+
+
+
+
